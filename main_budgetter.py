1d0234f5a59162253d659d83f1726a0b5bb65513
s s <samuel.dong.nz@gmail.com>
2025-09-07 21:14:38 +1200

    creating main_budgetter.py

---
+++ main_budgetter.py
@@ -0,0 +1,31 @@
+# import packages
+import pandas as pd
+import pyodbc
+from typing import TypedDict
+import json
+
+class Costs(TypedDict):
+    food: int
+    rent: int
+    sharesies: int
+    clothes: int
+
+class Budgetter():
+    def __init__(self, total_cash, costs: Costs):
+        self.total_cash = total_cash
+        self.costs = costs
+    
+    def getFood(self):
+        return self.costs['food']
+    
+    def getSum(self):
+        return 
+
+
+if __name__ == "__main__":
+    mycosts1: Costs = {'food' : 200, 'rent': 300, 'sharesies': 100, 'clothes': 50}
+    myBudget = Budgetter(20, mycosts1)
+    food_price = myBudget.getFood()
+    print(food_price)
+
+        
